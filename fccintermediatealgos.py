65a8d6949eaf9c65b6cab3dafeb2794818b01990
ColeTobe <nicoletobe2000@gmail.com>
2019-05-26 04:18:59 +0400

    added destroyer function

--- fccintermediatealgos.py
+++ fccintermediatealgos.py
@@ -1,5 +1,8 @@
-def destroyer(arr):
-    args = locals()
-    print(args)
-    
-# destroyer([1,2,3,4,5],3,3)
+def destroyer(arr, clear):
+    length = len(arr)
+    for item in range(length):
+        if item in clear:
+            arr.remove(item)
+    print(arr)
+
+destroyer([1,2,3,4,5],[3,2,4])
