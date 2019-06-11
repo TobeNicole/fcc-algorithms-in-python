ca0ad3b423d46008c6a97d8214c81284db29648a
ColeTobe <nicoletobe2000@gmail.com>
2019-06-11 23:53:08 +0400

    added pig latin and sum primes

--- fccintermediatealgos.py
+++ fccintermediatealgos.py
@@ -17,6 +17,49 @@ def what_is_in_a_name(collection, source):
                                 match_count += 1
                 if match_count == len(keys):
                         arr.append(item)
-        print(arr)
+        print(arr) 
 
-what_is_in_a_name([{ 'first': "Romeo", 'last': "Montague" }, { 'first': "Mercutio", 'last': 'null' }, { 'first': "Tybalt", 'last': "Capulet" }], { 'last': "Capulet" })
+# what_is_in_a_name([{ 'first': "Romeo", 'last': "Montague" }, { 'first': "Mercutio", 'last': 'null' }, { 'first': "Tybalt", 'last': "Capulet" }], { 'last': "Capulet" })
+
+def is_prime(num):
+  for i in range(2,num, 1):
+    if (num % i == 0):
+      print(False) 
+      return
+  print(True)
+  return
+# is_prime(20)
+
+
+def pig_latin(str):
+        vowels = ['a','e','i','o','u']
+        consonant = True if str[0] not in vowels else False
+        suffix = 'ay' if consonant else 'way'
+        for s in str:
+                if s in vowels:
+                        insert = str[:str.index(s)]
+                        return str[str.index(s):] + insert + suffix
+
+# print( pig_latin('glove'))
+
+
+def sum_primes(num):
+        total = 0
+        i = 2
+        prime = False
+        while i <= num:
+                for divisor in range(2, (i+1), 1): 
+                        if i % divisor == 0 and i != divisor:
+                                i+=1
+                                break
+                        elif i == divisor:
+                                prime = True
+                                break     
+                if prime:
+                        total += i
+                        i+=1
+                        prime = False
+        return total
+
+
+print(sum_primes(10))
